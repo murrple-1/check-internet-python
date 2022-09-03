57ad91fe2b85cfa3466b4f1f4e5edba4a1e68844
Murray Christopherson <murraychristopherson@gmail.com>
2022-09-03 15:21:36 -0700

    rewrite to not have any third-party dependencies

--- check_internet.py
+++ check_internet.py
@@ -1,44 +1,40 @@
 import sys
 import time
 import argparse
+import urllib.request
+import urllib.error
 
-import requests
 
-
-def internet_on(reference, timeout):
-    print('Trying to connect to \'{}\''.format(reference), file=sys.stderr)
+def internet_on(reference_url: str, timeout: float):
+    print(f"Trying to connect to '{reference_url}'", file=sys.stderr)
     try:
-        requests.head(reference, timeout=timeout)
+        request = urllib.request.Request(reference_url, method="HEAD")
+        with urllib.request.urlopen(request, timeout=timeout):
+            pass
         return True
-    except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
+    except urllib.error.URLError:
         return False
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '-r',
-        '--reference-url',
-        default='http://142.250.188.46')
-    parser.add_argument('-t', '--timeout', type=float, default=1)
-    parser.add_argument('-s', '--sleep-timeout', type=float, default=5)
+    parser.add_argument("-r", "--reference-url", default="http://142.250.188.46")
+    parser.add_argument("-t", "--timeout", type=float, default=1)
+    parser.add_argument("-s", "--sleep-timeout", type=float, default=5)
 
     args = parser.parse_args()
 
     try:
         while not internet_on(args.reference_url, args.timeout):
-            print('Internet Down...', file=sys.stderr)
-            print(
-                'Sleeping for {} seconds'.format(
-                    args.sleep_timeout),
-                file=sys.stderr)
+            print("Internet Down...", file=sys.stderr)
+            print("Sleeping for {} seconds".format(args.sleep_timeout), file=sys.stderr)
             time.sleep(args.sleep_timeout)
 
-        print('\a')
-        print('Internet back up!', file=sys.stderr)
+        print("\a")
+        print("Internet back up!", file=sys.stderr)
     except KeyboardInterrupt:
         pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
